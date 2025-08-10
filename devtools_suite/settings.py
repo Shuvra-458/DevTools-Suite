4a54b20e59a29ad1c14c2d9054b2c8df79abdb4a
Shuvrajyoti <shuvra458@gmail.com>
2025-08-10 18:37:08 +0530

    Update settings.py

--- devtools_suite/settings.py
+++ devtools_suite/settings.py
@@ -32,6 +32,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://dev-tools-suite.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
